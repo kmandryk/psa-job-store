e041db501ccbe0fb21b70498006aba666a8f8cc6
Mandryk <Kaegan.Mandryk@gov.bc.ca>
2024-01-30 14:36:39 -0800

    D

---
+++ .github/actions/migrate-db/migrate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ENVIRONMENT=$1
+OPENSHIFT_SERVER=$2
+DEV_TOKEN=$3
+TEST_TOKEN=$4
+PROD_TOKEN=$5
+
+#Log in to OpenShift
+echo "Deploying to $ENVIRONMENT"
+case "$ENVIRONMENT" in
+    "dev")
+    token=$DEV_TOKEN
+    ;;
+    "test")
+    token=$TEST_TOKEN
+    ;;
+    "prod")
+    token=$PROD_TOKEN
+    ;;
+    *)
+    echo "Unknown environment: $ENVIRONMENT"
+    exit 1
+    ;;
+esac
+
+NAMESPACE="c0cce6-$ENVIRONMENT"
+
+oc login $OPENSHIFT_SERVER --token=$token --insecure-skip-tls-verify=true
+oc project $NAMESPACE
+
+#set up port forwarding to access the database
+DATABASE_POD_NAME=$(oc get pods -n $NAMESPACE -l name=api-postgres -o jsonpath='{.items[0].metadata.name}')
+oc port-forward $DATABASE_POD_NAME 5432 &
+
+#run the db migration
+npx -w api prisma migrate deploy
